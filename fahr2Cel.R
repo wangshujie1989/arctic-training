08b475f5d2904deb8f11eb757b675b9c55a29733
Shujie Wang <wangshujie1989@gmail.com>
2019-01-17 19:10:21 -0500

    create function

---
+++ fahr2Cel.R
@@ -0,0 +1,17 @@
+
+
+
+
+fahr_to_celcius <- function(fahr){
+   celcius <- (fahr-32)*5/9
+   return (celcius)
+  
+}
+
+
+
+celcius_to_fahr <- function (celcius){
+  
+  fahr<-9*celcius/5+32
+  return(fahr)
+}
